c983056810de1649353bb5ffc5ff9230d156ddda
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-05 22:58:55 +0300

    Create app

---
+++ src/main/java/com/dennkk/aiod/AiodApplication.java
@@ -0,0 +1,13 @@
+package com.dennkk.aiod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiodApplication.class, args);
+	}
+
+}
