e38137f05705936cb86939643a9fe0ba527e945a
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-11 15:47:32 +0300

    Create user service

---
+++ src/main/java/com/dennkk/aiod/services/UserService.java
@@ -0,0 +1,21 @@
+package com.dennkk.aiod.services;
+
+import com.dennkk.aiod.repos.UserRepo;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+    private final UserRepo userRepo;
+
+    public UserService(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepo.findByUsername(username);
+    }
+}
