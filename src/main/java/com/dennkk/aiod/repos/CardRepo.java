de97ac567f76f93b127808306fe139a60e5a9567
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-11 15:40:46 +0300

    Create card repo

---
+++ src/main/java/com/dennkk/aiod/repos/CardRepo.java
@@ -0,0 +1,10 @@
+package com.dennkk.aiod.repos;
+
+import com.dennkk.aiod.domain.CardEntity;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CardRepo extends CrudRepository<CardEntity, Long> {
+    List<CardEntity> findByTagsIn(List<String> tags);
+}
