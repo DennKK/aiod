080270621c92750a13ebfeb57aaa735db1375b69
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-11 15:39:56 +0300

    Create card controller

---
+++ src/main/java/com/dennkk/aiod/controllers/CardController.java
@@ -0,0 +1,71 @@
+package com.dennkk.aiod.controllers;
+
+import com.dennkk.aiod.domain.CardEntity;
+import com.dennkk.aiod.domain.UserEntity;
+import com.dennkk.aiod.repos.CardRepo;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class CardController {
+    private final CardRepo cardRepo;
+
+    public CardController(CardRepo cardRepo) {
+        this.cardRepo = cardRepo;
+    }
+
+    @GetMapping("/cards")
+    public String cards(@RequestParam(required = false) String tags, Model model) {
+        List<String> tagsList;
+        Iterable<CardEntity> allPosts;
+
+        if (tags != null && !tags.isEmpty()) {
+            tagsList = Arrays.asList(tags.split(" "));
+            allPosts = cardRepo.findByTagsIn(tagsList);
+        } else {
+            allPosts = cardRepo.findAll();
+        }
+        model.addAttribute("cards", allPosts);
+        return "cards_view";
+    }
+
+    @GetMapping("/cards/create")
+    public String createCard() {
+        return "createCard_view";
+    }
+
+    @PostMapping("/cards/create")
+    public String createCard(
+            @AuthenticationPrincipal UserEntity author,
+            @RequestParam String name,
+            @RequestParam String preview,
+            @RequestParam String description,
+            @RequestParam String tags,
+            @RequestParam String link
+    ) {
+        List<String> tagsList = Arrays.asList(tags.split(" "));
+
+        CardEntity newCard = new CardEntity(name, preview, description, tagsList, link, author);
+        cardRepo.save(newCard);
+        return "redirect:/cards";
+    }
+
+    @GetMapping("/card/{id}")
+    public String getPost(@PathVariable Long id, Model model) {
+
+        CardEntity card = cardRepo.findById(id).orElse(null);
+        if (card != null) {
+            model.addAttribute("card", card);
+            return "cardProfile_view";
+        }
+        return "cards_view";
+    }
+}
