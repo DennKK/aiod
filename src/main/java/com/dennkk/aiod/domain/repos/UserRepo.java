da21ec7352192e349d4474677b61b2a71611df2f
Denis Karpov <karpovdenis.al@gmail.com>
2024-04-01 13:47:40 +0300

    Add optional to find by username

--- src/main/java/com/dennkk/aiod/domain/repos/UserRepo.java
+++ src/main/java/com/dennkk/aiod/domain/repos/UserRepo.java
@@ -3,6 +3,8 @@ package com.dennkk.aiod.domain.repos;
 import com.dennkk.aiod.domain.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<UserEntity, Long> {
-    UserEntity findByUsername(String username);
+    Optional<UserEntity> findByUsername(String username);
 }
